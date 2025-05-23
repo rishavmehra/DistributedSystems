e9f239820bb566539b28d5b9f7504bd1e35e8d17
rishavmehra <rishavmehra61@gmail.com>
2025-05-23 12:02:52 +0530

    deadlock

---
+++ Concurrency/Deadlock/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// For a deadlock to occur, four conditions, known as the Coffman conditions, must be met simultaneously: mutual exclusion, hold and wait, no preemption, and circular wait.
+
+func main() {
+	var wg sync.WaitGroup
+	var mu1, mu2, mu3 sync.Mutex
+
+	wg.Add(3)
+
+	go func() {
+		defer wg.Done()
+		mu1.Lock()
+		fmt.Println("goroutine Acquired lock 1")
+		mu2.Lock()
+		fmt.Println("goroutine Acquired lock 2")
+		mu3.Lock()
+		fmt.Println("goroutine Acquired lock 3")
+	}()
+	go func() {
+		defer wg.Done()
+		mu2.Lock()
+		fmt.Println("goroutine Acquired lock 2")
+		mu3.Lock()
+		fmt.Println("goroutine Acquired lock 3")
+		mu1.Lock()
+		fmt.Println("goroutine Acquired lock 1")
+	}()
+	go func() {
+		defer wg.Done()
+		mu3.Lock()
+		fmt.Println("goroutine Acquired lock 3")
+		mu1.Lock()
+		fmt.Println("goroutine Acquired lock 1")
+		mu2.Lock()
+		fmt.Println("goroutine Acquired lock 2")
+	}()
+	wg.Wait()
+}
