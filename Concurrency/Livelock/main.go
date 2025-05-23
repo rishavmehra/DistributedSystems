f1b66abe38be09b302ea2be47ff5547c5d822643
rishavmehra <rishavmehra61@gmail.com>
2025-05-23 14:04:03 +0530

    livelock

---
+++ Concurrency/Livelock/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	c1 := Chopstick{}
+	c2 := Chopstick{}
+
+	go dine("Rishav", &c1, &c2)
+	go dine("Harsh", &c2, &c1)
+	time.Sleep(time.Second * 1)
+}
+
+type Chopstick struct {
+	sync.Mutex
+}
+
+func dine(name string, left, right *Chopstick) {
+	for {
+		left.Lock()
+		fmt.Println(name, "pick up left chopstick")
+
+		time.Sleep(1 * time.Millisecond)
+
+		locked := tryLock(right)
+		if !locked {
+			left.Unlock()
+			fmt.Println(name, "put down the chopstick and try it again")
+			continue
+		}
+
+		fmt.Println(name, "is eating 🍽️")
+		time.Sleep(10 * time.Millisecond)
+
+		right.Unlock()
+		left.Unlock()
+		fmt.Println("finished eating put down both chopstick")
+		break
+
+	}
+}
+
+func tryLock(c *Chopstick) bool {
+	locked := make(chan struct{}, 1)
+	go func() {
+		c.Lock()
+		locked <- struct{}{}
+	}()
+
+	select {
+	case <-locked:
+		return true
+	case <-time.After(2 * time.Millisecond):
+		return false
+	}
+
+}
